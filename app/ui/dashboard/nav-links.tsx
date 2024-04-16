e9712221cd67e10f7b02efb7e7513c454210c0e2
giles <giles.bradshaw@sigyl.com>
2024-04-16 21:46:53 +0100

    .

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,7 +23,7 @@ const links = [
 ];
 
 export default function NavLinks() {
-  console.log('on the client')
+  console.log('on the client');
   const pathname = usePathname();
   return (
     <>
@@ -33,13 +33,12 @@ export default function NavLinks() {
           <Link
             key={link.name}
             href={link.href}
-            className={
-              clsx(
-                'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
-                {
-                  'bg-sky-100 text-red-600': pathname === link.href,
-                }
-              )}
+            className={clsx(
+              'flex h-[48px] grow items-center justify-center gap-2 rounded-md bg-gray-50 p-3 text-sm font-medium hover:bg-sky-100 hover:text-blue-600 md:flex-none md:justify-start md:p-2 md:px-3',
+              {
+                'bg-sky-100 text-red-600': pathname === link.href,
+              },
+            )}
           >
             <LinkIcon className="w-6" />
             <p className="hidden md:block">{link.name}</p>
